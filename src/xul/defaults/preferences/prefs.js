c195cdd0b0556c893cce0dc59327db350b32a874
Stephen Becker IV <sbeckeriv+git@gmail.com>
2012-01-08 08:54:02 -0800

    ref #2 added the block list url. I found another project that was turning off nextwork calls durning testing. I have yet to find away to completely turn off addons in the xulrunner code.

--- src/xul/defaults/preferences/prefs.js
+++ src/xul/defaults/preferences/prefs.js
@@ -12,4 +12,20 @@ pref("extensions.logging.enabled", true);
 pref("nglayout.debug.disable_xul_fastload", true);
 pref("dom.report_all_js_exceptions", true);
 pref("browser.xul.error_pages.enabled", true);
+// from http://hg.mozilla.org/mozilla-central/file/1dd81c324ac7/build/automation.py.in#l372 got this from https://github.com/mozilla/addon-sdk/commit/d916a4ce92168d16d36c193b61b7a4ddc2678ae1
+pref("extensions.enabledScopes", 5);
+pref("extensions.getAddons.cache.enabled", false);
+pref("extensions.installDistroAddons", false);
+pref("extensions.testpilot.runStudies", false);
+
+
+// crap from http://hg.mozilla.org/mozilla-central/file/1dd81c324ac7/build/automation.py.in#l372
+pref("dom.allow_scripts_to_close_windows", false)
+//prevents slow script window
+pref("dom.max_script_run_time", 0);
+// we dont need this
+pref('urlclassifier.updateinterval' , 172800);
+pref("app.update.enabled", false);
+pref("dom.w3c_touch_events.enabled", true);
+pref("extensions.blocklist.url", "chrome://crow/content/blocklist.xml");
 
